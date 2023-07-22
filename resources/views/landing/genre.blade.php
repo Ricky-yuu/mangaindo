2c229e8c9959805642c503983093866ac6205aa6
Ricky-yuu <meliodaskirisami@gmail.com>
2023-07-22 21:43:40 +0800

    nambah

---
+++ resources/views/landing/genre.blade.php
@@ -0,0 +1,88 @@
+@extends('landing.navbar')
+@section('navbar')
+
+<div class="genre-anime">
+    <div class="row">
+        <div class="container">
+    <ul>
+        <li>
+            <a href="">Action</a>
+        </li>
+        <li>
+            <a href="">Adventure</a>
+        </li>
+        <li>
+            <a href="">Comedy</a>
+        </li>
+        <li>
+            <a href="">Demons</a>
+        </li>
+        <li>
+            <a href="">Drama</a>
+        </li>
+        <li>
+            <a href="">Ecchi</a>
+        </li>
+        <li>
+            <a href="">Fantasy</a>
+        </li>
+        <li>
+            <a href="">Game</a>
+        </li>
+        <li>
+            <a href="">Harem</a>
+        </li>
+        <li>
+            <a href="">Historical</a>
+        </li>
+        <li>
+            <a href="">Horror</a>
+        </li>
+        <li>
+            <a href="">Josei</a>
+        </li>
+        <li>
+            <a href="">Magic</a>
+        </li>
+        <li>
+            <a href="">Martial Arts</a>
+        </li>
+        <li>
+            <a href="">Mecha</a>
+        </li>
+        <li>
+            <a href="">Military</a>
+        </li>
+        <li>
+            <a href="">Music</a>
+        </li>
+        <li>
+            <a href="">Mystery</a>
+        </li>
+        <li>
+            <a href="">Parody</a>
+        </li>
+        <li>
+            <a href="">Police</a>
+        </li>
+        <li>
+            <a href="">Psychological</a>
+        </li>
+        <li>
+            <a href="">Romance</a>
+        </li>
+        <li>
+            <a href="">Samurai</a>
+        </li>
+        <li>
+            <a href="">School</a>
+        </li>
+        <li>
+            <a href="">Sci-Fi</a>
+        </li>
+    </ul>
+</div>
+</div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('landing/animelist');
+    return view('landing/genre');
 });
 Route::get('/admin', function () {
     return view('admin/Dashboard');
